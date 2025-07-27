6508f9482e746db3b4d6ed358518921a731311a7
MRIDULA-0-9 <mridula09042006@gmail.com>
2025-07-27 19:46:25 +0530

    Delete Day4_task directory

--- Day4_task/Task1.js
+++
@@ -1,6 +0,0 @@
-let sentence = "Hello world";
-let result = sentence
-  .split(" ")
-  .map(w => w.split("").reverse().join(""))
-  .join(" ");
-console.log(result);
--- Day4_task/Task3.js
+++
@@ -1,9 +0,0 @@
-let str = "Hello World";
-let count = 0;
-let vowels = "aeiouAEIOU";
-for (let char of str) {
-  if (vowels.includes(char)) {
-    count++;
-  }
-}
-console.log("Number of vowels:", count); 

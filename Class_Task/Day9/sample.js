83063010e40446ddebe687c2d7579a6c8fbaa831
MRIDULA-0-9 <mridula09042006@gmail.com>
2025-09-03 09:48:09 +0530

    Add files via upload

---
+++ Class_Task/Day9/sample.js
@@ -0,0 +1,9 @@
+//Import and export modules
+
+//Definition:
+//Export allows you to make variables,functions or classes available to other files
+
+//Import allows you to bring those into another file
+
+//Used in modular JavaScript (ES6+)
+
